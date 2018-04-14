b85e93fbd968e3a95501b589b97ce31d5aef1619
ExcViral <exc.viral@gmail.com>
2018-04-14 15:41:57 +0530

    Created folder "Component Test Codes" This folder will contain all test codes for the individual components used in the robot Added a Readme file, also added component test for Ultrasonic sensors

---
+++ Component Test Codes/UltrasoninSensors/UltrasoninSensors.ino
@@ -0,0 +1,23 @@
+/*
+* This code is to test if the ultrasonic sensors are working
+* as expected.
+* In our robot, we have three ultrasonic sensors, one on the
+* front, and one each on left and right side of the robot.
+* The ultrasonic sensors have to detect if there is a wall
+* present at respective side of the robot.
+* current setting of the robot as of 14 Apr 2018:
+*   Chassis dimensions: 13 cm x 15 cm
+*   Expected single block dimensions of maze: 25 cm x 25 cm
+* So, if we place the robot at the centre of the centre of 
+* the block, we need the clearance of 6 cm each on left &
+* right side, and 6 cm clearance on the front.
+*/
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
